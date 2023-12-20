cc2e0cb8e96319e481a1eb881a7f71fb19ce2613
ayna09 <154412134+ayna09@users.noreply.github.com>
2023-12-20 16:36:23 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -7,6 +7,18 @@ st.header("My first Streamlit App")
 option = st.sidebar.selectbox(
     'Select a mini project',
      ['line chart','map','T n C'])
+
+if option=='line chart':
+    chart_data = pd.DataFrame(
+      np.random.randn(20, 3),
+      columns=['a', 'b', 'c'])
+    st.line_chart(chart_data)
+elif option=='map':
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [3.1242, 101.6861],
+    columns=['lat', 'lon'])
+    st.map(map_data)
+
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
 if show:
